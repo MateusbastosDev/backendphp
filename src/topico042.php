29c8cc51c25dfb706811d097327a1d860d8cd9b9
DeveloperMateus <mateusbastossdev@gmail.com>
2026-03-26 00:50:46 +0000

    topico 04

---
+++ src/topico042.php
@@ -0,0 +1,14 @@
+<?php
+$a=10;
+$b=2;
+$a+=$b;//12 variavel esquerdo sempre sera o resultado a=12
+$b-=5;//-3 
+echo "a = ".$a;
+echo "<br>b = ".$b;
+$c=11;
+$d=6;
+$c%=$d;//$c=11%//$c=5
+$d+=$a;//$d=6+12//$d=18
+echo "<br>c = ".$c;//c = 5
+echo "<br>d = ".$d;//d = 18
+?>
